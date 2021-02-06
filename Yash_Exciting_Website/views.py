cbbf0b1f851dd8f3b52f288be5f33464d34bb74b
Yash Varshney <74306688+Yash-Varshney-Creativities@users.noreply.github.com>
2021-02-06 17:33:50 +0530

    Edited Things and Improved

--- Yash_Exciting_Website/views.py
+++ Yash_Exciting_Website/views.py
@@ -12,6 +12,7 @@ def sent(request):
     mail = smtplib.SMTP('smtp.gmail.com', 587)
     mail.ehlo()
     mail.starttls()
+    mail.ehlo_or_helo_if_needed()
     mail.login('yashpythonemail@gmail.com', 'ijarileucauossqo')
     Name = request.GET.get('Name', 'default')
     Name = Name.rstrip()
@@ -22,7 +23,6 @@ def sent(request):
     howDidYouGetHere = request.GET.get('howGethere', 'default')
     howDidYouGetHere  = howDidYouGetHere.rstrip()
     howDidYouGetHere  = howDidYouGetHere.lstrip()
-
     mail.sendmail('yashpythonemail@gmail.com', 'yash.gurukul12@gmail.com', f'Subject: Mail from {Name} [Yash\'s Django Website]\n\nHi Yash, This is a Website mail bot from Python AnyWhere.com\n\n(Message From User is) - \n{User_Message}\n\n(How did You Get Here) - \n{howDidYouGetHere}\n\nThanks\n\tNote-This is a mail sent with Python Automation. If got this by mistake, please ignore.')
     mail.quit()
-    return HttpResponse('Sent')
+    return render(request, 'sent.html')
